9c14b26bb9c335721ea28aa71edd1ac58e09550a
James Livas <jlivas@ontech.com.au>
2014-10-31 04:59:41 +0000

    add license headers

--- package/app/code/community/Codisto/Sync/controllers/IndexController.php
+++ package/app/code/community/Codisto/Sync/controllers/IndexController.php
@@ -1,7 +1,24 @@
 <?php
-
-  class Codisto_Sync_IndexController extends Mage_Core_Controller_Front_Action
-  {
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * @category    Codisto
+ * @package     Codisto_Sync
+ * @copyright   Copyright (c) 2012 On Technology (http://www.ontech.com.au)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class Codisto_Sync_IndexController extends Mage_Core_Controller_Front_Action
+{
 	public function indexAction()
 	{
 	
@@ -307,5 +324,5 @@ syslog(1, 'CODISTO ORDERID: ' . $neworder->getCodistoOrderid());
 		$regionCollection = Mage::getModel('directory/region_api')->items($countryCode);
 		return $regionCollection;
 	}
-  }
+}
 ?>
