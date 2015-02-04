255d615424b4a62f855db1b6ffc708218a90d23e
Brad McCormack <bradmccormack@exemail.com.au>
2015-02-04 05:59:25 +0000

    - wip

--- code/community/Codisto/Sync/Model/Observer.php
+++ code/community/Codisto/Sync/Model/Observer.php
@@ -25,34 +25,33 @@ class Codisto_Sync_Model_Observer
 
 	public function salesOrderShipmentSaveAfter(Varien_Event_Observer $observer)
 	{
-		if (!$observer->getEvent()->getBlock()->getIsSecureMode()) {
-			syslog(LOG_INFO, "Not secure");
-			return;
-		} else {
-			syslog(LOG_INFO, "Is secure - continuing");
-		}
-
 		$shipment = $observer->getEvent()->getShipment();
 		$order = $shipment->getOrder();
 		$orderid = $order->getIncrementId();
+		syslog("Orderid is " . $orderid);
 
-		$remoteUrl = "https://ui.codisto.com/setebayfeedback";
-		//$MerchantID = Mage::getStoreConfig('codisto/merchantid');
+		$MerchantID = Mage::getStoreConfig('codisto/merchantid');
 		//$HostID = Mage::getStoreConfig('codisto/hostid');
 		$HostKey = Mage::getStoreConfig('codisto/hostkey');
 
+		syslog(LOG_INFO, "MerchantID is ". $MerchantID);
+		$remoteUrl = 'https://ui.codisto.com/' . $MerchantID . '/setebayfeedback';
+
+
 		$client = new Zend_Http_Client($remoteUrl, array( 'keepalive' => true ));
+		$client->setUri($remoteUrl);
 		if($HostKey)
 			$client->setHeaders(array('X-HostKey' => $HostKey));
 
 		$baseurl = Mage::getBaseUrl();
 		//$userid = Mage::getSingleton('admin/session')->getUser()->getId();
 
-		syyslog("Sending request to update ebayfeedback");
-		$remoteResponse = $client->setRawData('{"type" : "magentoplugin","baseurl" : "' . $baseurl . '", "orderid" :' . $orderid .'}', 'application/json')->request('POST');
+		syslog(LOG_INFO, "Sending request to update ebayfeedback");
+		$remoteResponse = $client->setRawData('{"action" : "setebayfeedback" , "type" : "magentoplugin","baseurl" : "' . $baseurl . '", "orderid" :' . $orderid .'}', 'application/json')->request('POST');
 
 		//$data = json_decode($remoteResponse->getRawBody(), true);
 
+		syslog(LOG_INFO, print_r($remoteResponse, 1));
 
 		return $this;
 	}
