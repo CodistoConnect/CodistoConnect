dad7fc94cd6379b38548b939618e50e59e9d56dc
Luke Amery <lamery@ontech.com.au>
2015-05-25 16:42:58 +1000

    bump copyright year

--- code/community/Codisto/Sync/Model/Resource/Mysql4/Setup.php
+++ code/community/Codisto/Sync/Model/Resource/Mysql4/Setup.php
@@ -14,10 +14,10 @@
  *
  * @category    Codisto
  * @package     Codisto_Sync
- * @copyright   Copyright (c) 2014 On Technology Pty. Ltd. (http://codisto.com/)
+ * @copyright   Copyright (c) 2015 On Technology Pty. Ltd. (http://codisto.com/)
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
- 
+
 class Codisto_Sync_Model_Resource_Mysql4_Setup extends Mage_Core_Model_Resource_Setup {
 
 }
