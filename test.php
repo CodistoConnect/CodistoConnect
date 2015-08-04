f34702a990f4ef0c54b5e894093cb8e81532f5ff
brad mccormack <brad_mccormack@exemail.com.au>
2015-08-04 07:21:55 +0000

    - Testing

--- test.php
+++ test.php
@@ -1,11 +1,16 @@
 <?php
+	@codingStandardsIgnoreStart
 	http_response_code(500);
+	@codingStandardsIgnoreEnd
+
 	/* Adding a change to test pull request hook */
 
 // @codingStandardsIgnoreStart
 	http_response_code(500);
 // @codingStandardsIgnoreEnd
 
+	@codingStandardsIgnoreStart
 	http_response_code(500);
+	@codingStandardsIgnoreEnd
 ?>
 
