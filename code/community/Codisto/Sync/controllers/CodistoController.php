c856f2f3be80f8a18dc1d5ce73459cabfcab48e2
James <jlivas@ontech.com.au>
2015-12-31 02:19:25 +0000

    remove unneeded action

--- code/community/Codisto/Sync/controllers/CodistoController.php
+++ code/community/Codisto/Sync/controllers/CodistoController.php
@@ -40,19 +40,6 @@ class Codisto_Sync_CodistoController extends Mage_Adminhtml_Controller_Action
 		$this->renderLayout();
 	}
 	
-	public function categoriesAction()
-	{
-		
-		$url = preg_replace('/\/index\/key\//', '/categories/key/', Mage::getModel('adminhtml/url')->getUrl('codisto/ebaytab'));
-
-		$this->loadLayout();
-
-		$block = $this->getLayout()->createBlock('core/text', 'green-block')->setText('<div id="codisto-control-panel-wrapper"><iframe id="codisto-control-panel" class="codisto-iframe codisto-bulk-editor" src="'. $url . '" frameborder="0" onmousewheel=""></iframe></div>');
-		$this->_addContent($block);
-
-		$this->renderLayout();
-	}
-
 	public function introAction()
 	{
 		
