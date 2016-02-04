750b35f653fe84222351c41a47b5495f24471590
brad mccormack <brad_mccormack@exemail.com.au>
2016-02-04 18:10:15 +1100

    - Mitigate timing attacks against hash and fallback to MIT licensed version for older php

--- code/community/Codisto/Sync/Helper/Data.php
+++ code/community/Codisto/Sync/Helper/Data.php
@@ -18,6 +18,54 @@
 * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
 */
 
+
+if (!function_exists('hash_equals')) {
+
+	function hash_equals($known_string, $user_string)
+	{
+
+		/**
+		 * This file is part of the hash_equals library
+		 *
+		 * For the full copyright and license information, please view the LICENSE
+		 * file that was distributed with this source code.
+		 *
+		 * @copyright Copyright (c) 2013-2014 Rouven Weßling <http://rouvenwessling.de>
+		 * @license http://opensource.org/licenses/MIT MIT
+		 */
+
+		// We jump trough some hoops to match the internals errors as closely as possible
+		$argc = func_num_args();
+        $params = func_get_args();
+
+        if ($argc < 2) {
+            trigger_error("hash_equals() expects at least 2 parameters, {$argc} given", E_USER_WARNING);
+            return null;
+        }
+
+        if (!is_string($known_string)) {
+        	trigger_error("hash_equals(): Expected known_string to be a string, " . gettype($known_string) . " given", E_USER_WARNING);
+            return false;
+        }
+        if (!is_string($user_string)) {
+        	trigger_error("hash_equals(): Expected user_string to be a string, " . gettype($user_string) . " given", E_USER_WARNING);
+            return false;
+        }
+
+        if (strlen($known_string) !== strlen($user_string)) {
+        	return false;
+        }
+		$len = strlen($known_string);
+		$result = 0;
+        for ($i = 0; $i < $len; $i++) {
+            $result |= (ord($known_string[$i]) ^ ord($user_string[$i]));
+        }
+        // They are only identical strings if $result is exactly 0...
+        return 0 === $result;
+	}
+}
+
+
 class Codisto_Sync_Helper_Data extends Mage_Core_Helper_Abstract
 {
 	public function getCodistoVersion()
@@ -35,7 +83,7 @@ class Codisto_Sync_Helper_Data extends Mage_Core_Helper_Abstract
 			$base = hash('sha256', $r, true);
 			$checkHash = base64_encode($base);
 
-			$hashOK = $Hash == $checkHash;
+			$hashOK = hash_equals($Hash ,$checkHash);
 		}
 
 		return $hashOK;
