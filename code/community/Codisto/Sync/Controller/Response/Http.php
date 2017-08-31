796f7edd5251faf98860dc4513a202295d33f2c0
Luke Amery <lamery@ontech.com.au>
2017-08-31 11:51:50 +1000

    add controller response

---
+++ code/community/Codisto/Sync/Controller/Response/Http.php
@@ -0,0 +1,28 @@
+<?php
+/**
+* Codisto eBay Sync Extension
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Open Software License (OSL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/osl-3.0.php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to license@magentocommerce.com so we can send you a copy immediately.
+*
+* @category    Codisto
+* @package     Codisto_Sync
+* @copyright   Copyright (c) 2015 On Technology Pty. Ltd. (http://codisto.com/)
+* @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+*/
+
+class Codisto_Sync_Controller_Response_Http extends Mage_Core_Controller_Response_Http {
+
+    public function sendHeaders()
+    {
+        return $this;
+    }
+
+}
